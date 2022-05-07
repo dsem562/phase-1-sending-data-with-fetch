ea617e939cbc634b1252313e8c64cabb3427b86f
David <dsem562@gmail.com>
2022-05-07 12:06:47 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+
+function submitData(name, email) {
+  const body = document.body;
+  
+  return fetch("http://localhost:3000/users", configurationObject(name, email))
+  .then(function (response) {          
+    return response.json();
+  })
+  .then(function (object) {        
+    body.innerHTML = object.id;
+  })
+  .catch(function (error) {
+    body.innerHTML = error.message;
+  });
+  
+  };
+
+  function configurationObject(name, email) {
+    let object = { method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    }),
+  }
+  return object
+};
+
+submitData();
